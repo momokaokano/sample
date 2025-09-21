0e651ddaf8904a08119b22f25f939a462694eaab
momokaokano <momoka.okano@d-ss.co.jp>
2025-09-21 17:18:24 +0900

    initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.name_prefix}-${random_id.suffix.hex}"
+  acl    = "private"
+  tags = {
+    Name = var.name_prefix
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
